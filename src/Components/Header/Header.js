55ac76d019bcfebfcb58dc0097824c5cfdd9449d
octaviofitz <octaviofitz@gmail.com>
2025-01-06 18:58:46 -0300

    tests

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,7 +5,7 @@ import './header.css';
 function Header() {
     return (
         <div className='header'>
-            <h1 className='titulo'>MEGASTEEL: <br></br> ARQUITECTURA INDUSTRIAL</h1>
+            <h1 className='titulo'>MEGASTEEL<br></br> ARQUITECTURA INDUSTRIAL</h1>
      </div>
     );
 }
