9504c22a0eee3c40128499bcdeec38f09a5a768e
octaviofitz <octaviofitz@gmail.com>
2025-01-06 18:50:09 -0300

    change colors and fonts

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import logo from '../../IMG/Index/Isologo.png';
+
 import './footer.css';
 
 function Footer() {
     return (
         <footer className='footer'>
             <div className='container'>
+                <img src={logo} className='logo' alt='Logo Megasteel' />
             <p className='links'>© 2024 Megasteel. Todos los derechos reservados</p>
             <p className='links'>Desarrollado por Galidé</p>
             </div>
