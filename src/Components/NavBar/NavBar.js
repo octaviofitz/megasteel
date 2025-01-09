0aae4146c9c98025b9c7097f3b5d0f3ccfe58446
octaviofitz <octaviofitz@gmail.com>
2025-01-09 17:29:32 -0300

    navbar bootstrap

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,144 +1,44 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import MenuIcon from '@mui/icons-material/Menu';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-
-import Isologo from '../../IMG/Index/Isologo.png';
-import Logo from '../../IMG/Index/Logo.png';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link, Button, Element, Events, animateScroll as scroll, scrollSpy } from 'react-scroll';
 
 import './navBar.css';
+import Logo from '../../IMG/Index/Logo.png';
 
-const drawerWidth = 240;
-const navItems = [
-  { label: 'Nosotros', href: '#nosotros' },
-  { label: 'Trabajos', href: '#trabajos' },
-  { label: 'Contacto', href: '#contacto' },
-];
-
-function DrawerAppBar(props) {
-  const { window } = props;
-  const [mobileOpen, setMobileOpen] = React.useState(false);
-
-  const handleDrawerToggle = () => {
-    setMobileOpen((prevState) => !prevState);
-  };
-
-  const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
-      {/* Logo en el menú móvil */}
-      <Typography variant="h6" sx={{ my: 2 }}>
-        <img
-          src={Isologo}
-          alt="MEGASTEEL Logo"
-          style={{ height: '40px', cursor: 'pointer' }}
-          className="isologo"
-        />
-      </Typography>
-      <Divider />
-      <List>
-        {navItems.map((item) => (
-          <ListItem key={item.label} disablePadding>
-            <ListItemButton
-              sx={{ textAlign: 'center' }}
-              component="a"
-              href={item.href} // Navega a la sección correspondiente
-            >
-              <ListItemText primary={item.label} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
-  );
-
-  const container = window !== undefined ? () => window().document.body : undefined;
-
+function BasicExample() {
   return (
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <AppBar component="nav" className="navBar">
-        <Toolbar>
-          {/* Logo en la barra de navegación */}
-          <Typography
-            variant="h6"
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: 'block', sm: 'block' } }}
-          >
-            <img
-              src={Logo}
-              alt="MEGASTEEL Logo"
-              style={{ height: '40px', cursor: 'pointer' }}
-              className="logo"
-            />
-          </Typography>
-
-          {/* Botón de menú hamburguesa */}
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="end"
-            onClick={handleDrawerToggle}
-            sx={{ display: { xs: 'block', sm: 'none' } }}
-          >
-            <MenuIcon />
-          </IconButton>
-
-          {/* Botones de navegación en desktop */}
-          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-            {navItems.map((item) => (
-              <Button
-                key={item.label}
-                sx={{ color: '#fff' }}
-                href={item.href} // Navega a la sección correspondiente
-              >
-                {item.label}
-              </Button>
-            ))}
-          </Box>
-        </Toolbar>
-      </AppBar>
-
-      <nav>
-        <Drawer
-          anchor="right"
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true,
-          }}
-          sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-          }}
-          className="menuDrawer"
-        >
-          {drawer}
-        </Drawer>
-      </nav>
-
-      <Box component="main">
-        <Toolbar />
-      </Box>
-    </Box>
+    <Navbar expand="lg" className="bg-body-tertiary navBar">
+      <Container
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        {/* Logo a la izquierda */}
+        <Navbar.Brand href="#home" style={{ flexShrink: 0 }}>
+          <img src={Logo} className="logo" alt="Logo" />
+        </Navbar.Brand>
+        
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="d-flex w-100 justify-content-lg-end">
+            <Nav.Link className="link" href="#nosotros">
+              Arquitectura Industrial
+            </Nav.Link>
+            <Nav.Link className="link" href="#portfolio">
+              Nuestros Trabajos
+            </Nav.Link>
+            <Nav.Link className="link" href="#contacto">
+              Contacto
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
-DrawerAppBar.propTypes = {
-  window: PropTypes.func,
-};
-
-export default DrawerAppBar;
+export default BasicExample;
